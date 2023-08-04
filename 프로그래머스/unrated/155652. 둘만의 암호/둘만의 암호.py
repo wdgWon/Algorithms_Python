624ac310c21f7908f2f311add8973770c5a2209d
wdgWon <128919388+wdgWon@users.noreply.github.com>
2023-08-04 10:56:49 +0900

    [unrated] Title: 둘만의 암호, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,12 @@
+def solution(s, skip, index):
+    alphabet = [chr(x) for x in range(97,123)]
+    result = ''
+    
+    for al in skip:
+        alphabet.remove(al)
+        
+    for al in s:
+        i = (alphabet.index(al) + index) % len(alphabet)
+        result += alphabet[i]
+    
+    return result
