aba0764cfd3d648c8f320c7e9978829d71631226
wdgWon <128919388+wdgWon@users.noreply.github.com>
2023-08-04 16:01:04 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,16 +1,12 @@
-from string import ascii_lowercase
-
 def solution(s, skip, index):
+    alphabet = [chr(x) for x in range(97,123)]
     result = ''
 
-    a_to_z = set(ascii_lowercase)
-    a_to_z -= set(skip)
-    a_to_z = sorted(a_to_z)
-    l = len(a_to_z)
+    for al in skip:
+        alphabet.remove(al)
 
-    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
-
-    for i in s:
-        result += a_to_z[(dic_alpha[i] + index) % l]
+    for al in s:
+        i = (alphabet.index(al) + index) % len(alphabet)
+        result += alphabet[i]
 
     return result
